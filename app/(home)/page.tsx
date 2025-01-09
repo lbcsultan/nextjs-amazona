566ba3cf103f45cfb8e6de7fb0d2ce402dea5113
Byoungcheon Lee <lbcsultan@gmail.com>
2025-01-09 14:10:23 +0900

    08-create-best-selling-slider

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -65,14 +65,27 @@ export default async function HomePage() {
 
   const todaysDeals = await getProductsByTag({ tag: 'todays-deal' })
 
+  const bestSellingProducts = await getProductsByTag({ tag: 'best-seller' })
+
   return (
     <>
       <HomeCarousel items={data.carousels} />
       <div className='md:p-4 md:space-y-4 bg-border'>
         <HomeCard cards={cards} />
+
         <Card className='w-full rounded-none'>
           <CardContent className='p-4 items-center gap-3'>
             <ProductSlider title={"Today's Deals"} products={todaysDeals} />
+          </CardContent>
+        </Card>
+
+        <Card className='w-full rounded-none'>
+          <CardContent className='p-4 items-center gap-3'>
+            <ProductSlider
+              title='Best Selling Products'
+              products={bestSellingProducts}
+              hideDetails
+            />
           </CardContent>
         </Card>
       </div>
