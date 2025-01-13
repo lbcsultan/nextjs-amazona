871f8cae1a6cbab6b2ec08d47e27403455f3a105
Byoungcheon Lee <lbcsultan@gmail.com>
2025-01-13 15:57:09 +0900

    23-update-user-name

--- lib/validator.ts
+++ lib/validator.ts
@@ -190,3 +190,7 @@ export const UserSignUpSchema = UserSignInSchema.extend({
   message: "Passwords don't match",
   path: ['confirmPassword'],
 })
+
+export const UserNameSchema = z.object({
+  name: UserName,
+})
